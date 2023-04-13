a65bc8fc1ff8be4f954a808ea0521100c1c2863a
Luis <louiscrack50@gmail.com>
2023-04-13 00:55:13 -0600

    creating initial structure for API Gateway and auth service

---
+++ microservices/auth-service/src/main/java/com/packages/auth/controller/AuthUserController.java
@@ -0,0 +1,39 @@
+package com.packages.auth.controller;
+
+import com.packages.auth.dto.AuthUserDto;
+import com.packages.auth.dto.TokenDto;
+import com.packages.auth.entity.AuthUser;
+import com.packages.auth.service.AuthUserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/auth")
+public class AuthUserController {
+
+    @Autowired
+    AuthUserService authUserService;
+
+    @PostMapping("/login")
+    public ResponseEntity<TokenDto> login(@RequestBody AuthUserDto dto) {
+        TokenDto tokenDto = authUserService.login(dto);
+        if(tokenDto == null) return ResponseEntity.badRequest().build();
+        return ResponseEntity.ok(tokenDto);
+    }
+
+    @PostMapping("/validate")
+    public ResponseEntity<TokenDto> validate(@RequestParam String token){
+        TokenDto tokenDto = authUserService.validate(token);
+        if(tokenDto == null) return ResponseEntity.badRequest().build();
+        return ResponseEntity.ok(tokenDto);
+    }
+
+    @PostMapping("/create")
+    public ResponseEntity<AuthUser> create(@RequestBody AuthUserDto dto){
+        AuthUser authUser = authUserService.save(dto);
+        if(authUser == null) return ResponseEntity.badRequest().build();
+        return ResponseEntity.ok(authUser);
+    }
+
+}
---
+++ microservices/auth-service/src/main/java/com/packages/auth/service/AuthUserService.java
@@ -0,0 +1,50 @@
+package com.packages.auth.service;
+
+import com.packages.auth.dto.AuthUserDto;
+import com.packages.auth.dto.TokenDto;
+import com.packages.auth.entity.AuthUser;
+import com.packages.auth.repository.AuthUserRepository;
+import com.packages.auth.security.JwtProvider;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+public class AuthUserService {
+
+    @Autowired
+    AuthUserRepository authUserRepository;
+
+    @Autowired
+    PasswordEncoder passwordEncoder;
+
+    @Autowired
+    JwtProvider jwtProvider;
+
+    public AuthUser save(AuthUserDto dto) {
+        Optional<AuthUser> user = authUserRepository.findByUsername(dto.getUsername());
+        if(user.isPresent()) return null;
+        String password = passwordEncoder.encode(dto.getPassword());
+        AuthUser authUser = AuthUser.builder().username(dto.getUsername()).password(password).build();
+        return authUserRepository.save(authUser);
+    }
+
+    public TokenDto login(AuthUserDto dto){
+        Optional<AuthUser> user = authUserRepository.findByUsername(dto.getUsername());
+        if(!user.isPresent()) return null;
+        if(passwordEncoder.matches(dto.getPassword(), user.get().getPassword()))
+            return new TokenDto(jwtProvider.createToken(user.get()));
+        return null;
+    }
+
+    public TokenDto validate(String token){
+        if(!jwtProvider.validate(token)) return null;
+        String username = jwtProvider.getUserNameFromToken(token);
+        if(!authUserRepository.findByUsername(username).isPresent())
+            return null;
+        return new TokenDto(token);
+    }
+
+}
